be0c4eb20ad1369cdc5698d88475470f53e666a7
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2024-12-24 23:20:12 +0530

    add icons

--- Frontend/src/Components/AddRecipies/AddRecipies.jsx
+++ Frontend/src/Components/AddRecipies/AddRecipies.jsx
@@ -1,30 +1,50 @@
 import React from 'react';
+import Header from "../Header/Header.jsx";
 
 const AddRecipies = () => {
     return (
         <div>
+            <Header/>
             <h1>Add Recipie</h1>
-            <form action="">
+            <form action="" className="addRecipieForm">
                 <div className="mb-3 row">
-                    <label for="staticEmail" className="col-sm-2 col-form-label">Title</label>
+                    <label for="staticEmail" className="col-sm-2 col-form-label">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar-fill" viewBox="0 0 16 16">
+                            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5h16V4H0V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5"/>
+                        </svg>
+                        Title</label>
                     <div className="col-sm-10">
                         <input type="text" className="form-control" id="staticEmail" value="email@example.com"/>
                     </div>
                 </div>
                 <div className="mb-3 row">
-                    <label for="inputPassword" className="col-sm-2 col-form-label">Ingredients</label>
+                    <label for="inputPassword" className="col-sm-2 col-form-label">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-basket2-fill" viewBox="0 0 16 16">
+                            <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1"/>
+                        </svg>
+                        Ingredients</label>
                     <div className="col-sm-10">
                         <textarea className="form-control" name="" id="" cols="30" rows="10"></textarea>
                     </div>
                 </div>
                 <div className="mb-3 row">
-                    <label for="inputPassword" className="col-sm-2 col-form-label">Steps</label>
+
+                    <label for="inputPassword" className="col-sm-2 col-form-label">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-card-list" viewBox="0 0 16 16">
+                            <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+                            <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8m0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0M4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
+                        </svg>
+                        Steps</label>
                     <div className="col-sm-10">
                         <textarea className="form-control" name="" id="" cols="30" rows="10"></textarea>
                     </div>
                 </div>
                 <div className="mb-3 row">
-                    <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Description</label>
+                    <label htmlFor="inputPassword" className="col-sm-2 col-form-label">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-text-paragraph" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m4-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5"/>
+                        </svg>
+                        Description</label>
                     <div className="col-sm-10">
                         <textarea className="form-control" name="" id="" cols="30" rows="10"></textarea>
                     </div>
