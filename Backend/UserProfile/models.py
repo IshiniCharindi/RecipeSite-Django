a2de9521c216be1929034be32491e46ff5f9cee8
Saru <zaruzarujan@gmail.com>
2025-01-25 18:59:13 +0530

    commit

--- Backend/UserProfile/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User # type: ignore
-from django.db import models # type: ignore
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
-
-    def __str__(self):
-        return self.user.username
--- Backend/UserProfile/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserProfileView, change_password
-
-urlpatterns = [
-    path('profile/', UserProfileView.as_view(), name='user-profile'),
-    path('change-password/', change_password, name='change-password'),
-]
